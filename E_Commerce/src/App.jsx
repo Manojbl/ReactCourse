0896dce985434b90f17a415c41314714bcd83ed1
Manojbl <manojshaiva555@gmail.com>
2025-11-07 22:45:36 +0530

    create multiple using react router

--- E_Commerce/src/App.jsx
+++ E_Commerce/src/App.jsx
@@ -1,11 +1,13 @@
+import {Routes , Route} from 'react-router'
 import { HomePage } from './Components/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
