01ff583d29fe03890730dea1b30855b6fee39693
Manojbl <manojshaiva555@gmail.com>
2025-11-07 23:17:38 +0530

    update links to match the router

--- E_Commerce/src/App.jsx
+++ E_Commerce/src/App.jsx
@@ -1,14 +1,14 @@
 import {Routes , Route} from 'react-router'
 import { HomePage } from './Components/HomePage'
+import { CheckoutPage } from './Components/CheckoutPage'
 import './App.css'
 
-function App() {
+ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
-  )
-}
-
-export default App
+  );
+  }
+  export default App;
