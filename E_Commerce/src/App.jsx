e4d2241ab22c42be6da708ab6248fcf317fcefce
Manojbl <manojshaiva555@gmail.com>
2025-11-08 23:25:56 +0530

    load item into cart

--- E_Commerce/src/App.jsx
+++ E_Commerce/src/App.jsx
@@ -21,8 +21,8 @@ import './App.css'
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart}  loadCart={loadCart} />} />
+      <Route index element={<HomePage cart={cart}  loadCart={loadCart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}   />} />
       <Route path="tracking" element={<TrackingPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
